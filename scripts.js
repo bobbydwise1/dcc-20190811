9a001befc4855845aade1262d4adc752e105dc2b
Robert Lee <bobbydwise1@hotmail.com>
2019-08-11 22:23:07 -0700

    Delete text.

--- scripts.js
+++ scripts.js
@@ -12,13 +12,6 @@ For example, given the input [2, 1, 2], we can hold 1 unit of water in the middl
 Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in the second, and 3 in the fourth index (we cannot hold 5 since it would run off to the left), so we can trap 8 units of water.
 */
 
-const makeCode = (yourString) => {
-
-}
-
-let test1 = 'AAAABBBCCDAA';
-let result1 = '4A3B2C1D2A';
-
 $(document).ready(function() {
 
   $('#form1').submit(function(event) {
