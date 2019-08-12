abb26a6ec305621ac3f3592bb9b2369180feedcd
Robert Lee <bobbydwise1@hotmail.com>
2019-08-11 23:01:01 -0700

    Attemp switch statement.

--- scripts.js
+++ scripts.js
@@ -12,6 +12,32 @@ For example, given the input [2, 1, 2], we can hold 1 unit of water in the middl
 Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in the second, and 3 in the fourth index (we cannot hold 5 since it would run off to the left), so we can trap 8 units of water.
 */
 
+/*
+
+*/
+
+const findWell = (yourArray) => {
+  let left = yourArray[0];
+  let center = yourArray[1];
+  let right = yourArray[2];
+  switch (center) {
+    case ((left > center) && (center < right)):
+    if (left === right) {return left}
+    else if (left < right) {return left}
+    else {return right}
+    break;
+    case ((left > center) || (center < right)):
+    return 0;
+    break;
+    default:
+    console.log('made it to default')
+    break;
+  }
+}
+
+let test1 = [2,1,2]
+console.log(findWell(test1))
+
 $(document).ready(function() {
 
   $('#form1').submit(function(event) {
